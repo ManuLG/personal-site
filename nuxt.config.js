9547a630cf6d531590cfa170a43b21d11f890c77
Manuel Lorenzo <idc.manuel@gmail.com>
2022-03-28 18:54:28 +0200

    Fix for static generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,9 +34,9 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
     dirs: [
-      'src/ui/elements', 
-      'src/ui/patterns',
-      'src/ui/templates',
+      '~/src/ui/elements', 
+      '~/src/ui/patterns',
+      '~/src/ui/templates',
     ]
   },
 
